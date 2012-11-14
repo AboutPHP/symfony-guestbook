b875417f63b271464b8de1d4d27681df3bf60c52
Полищук Сергей <spolischook@spolischook.(none)>
2012-11-14 17:33:08 +0200

    Add view to the Guestbook homepage view

--- src/Serge/GuestbookBundle/Controller/DefaultController.php
+++ src/Serge/GuestbookBundle/Controller/DefaultController.php
@@ -3,11 +3,25 @@
 namespace Serge\GuestbookBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
-    public function indexAction($name)
+    public function indexAction()
     {
-        return $this->render('GuestbookBundle:Default:index.html.twig', array('name' => $name));
+        $form = $this->createFormBuilder()
+            ->add('name', 'text')
+            ->add('email', 'email')
+            ->add('text', 'textarea')
+            ->getForm();
+
+        $em = $this->getDoctrine()->getManager();
+        $repo = $em->getRepository('GuestbookBundle:Entry');
+
+        $aEntry = $repo->findBy(array(), array("id" => "DESC"), 10);
+        return $this->render('GuestbookBundle:Default:index.html.twig', array(
+            'aEntry' => $aEntry,
+            'form' => $form->createView(),
+        ));
     }
 }
