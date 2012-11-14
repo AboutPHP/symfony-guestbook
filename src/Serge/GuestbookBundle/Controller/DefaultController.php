b5e7d0182702ae558c41bf30a8a45770d7aa2c4b
Полищук Сергей <spolischook@spolischook.(none)>
2012-11-14 12:35:57 +0200

    Write import data from 101fm DB to GuestBook DB

--- src/Serge/GuestbookBundle/Controller/DefaultController.php
+++ src/Serge/GuestbookBundle/Controller/DefaultController.php
@@ -3,14 +3,32 @@
 namespace Serge\GuestbookBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Serge\GuestbookBundle\Entity\Entry;
 
 class DefaultController extends Controller
 {
-    public function indexAction($name)
+    public function indexAction()
+    {
+        
+    }
+
+    public function importAction()
     {
         $em = $this->getDoctrine()->getManager();
-        $repo = $em->getRepository('entry');
+        $repo = $em->getRepository('GuestbookBundle:KasJcomments');
 
-        var_dump ($repo->findAll());
+        //fins all comments from
+        $comments = $repo->findBy(array ('objectId' => '69'));
+        foreach ($comments as $comment) {
+            $oEntry = new Entry();
+            $oEntry->setName($comment->getName());
+            $oEntry->setEmail($comment->getEmail());
+            $oEntry->setText($comment->getComment());
+            $em->persist($oEntry);
+        }
+        $em->flush();
+        return $this->render('GuestbookBundle:Default:index.html.twig', array(
+            'comments' => $comments,
+        ));
     }
 }
